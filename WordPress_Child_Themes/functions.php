e71a98cc97067aaa08c4a8057965d9ceb462d0fb
Michelle Levine <mjlevine18@gmail.com>
2016-04-02 12:31:20 -0700

    adding enqueueing for fontawesome to wordpress child theme functions file

--- WordPress_Child_Themes/functions.php
+++ WordPress_Child_Themes/functions.php
@@ -10,4 +10,13 @@ function theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+// Load Font Awesome
+add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
+function enqueue_font_awesome() {
+
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
+
+}
 ?>
